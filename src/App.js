76a298f15f42bfd1d46a238730a9ac11ccd2faa1
Jason Mortensen <ingrey1@gmail.com>
2021-08-30 21:47:23 -0700

    add cherry tree svg logo setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={process.env.PUBLIC_URL + 'images/logos/cherry-blossom-tree.svg'} alt="logo" />
         <p>Writing Tree</p>
       </header>
     </div>
