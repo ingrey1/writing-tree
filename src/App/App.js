f31870e7b8eddc83f883bec03817baa6d15817ab
Jason Mortensen <ingrey1@gmail.com>
2021-09-01 20:46:47 -0700

    update layout

--- src/App/App.js
+++ src/App/App.js
@@ -2,44 +2,34 @@ import { Grid, Image } from "semantic-ui-react";
 import { TopMenu } from "../TopMenu/TopMenu";
 import "./App.css";
 import WritingTreeHeader from "../components/WritingTreeHeader/WritingTreeHeader";
+import MainMenu from "../MainMenu/MainMenu";
 
 function App() {
   return (
-    <div className="App">
-      <Grid celled="internally">
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Grid.Row>
-              <TopMenu />
-            </Grid.Row>
-            <Grid.Row>
-              <WritingTreeHeader />
-            </Grid.Row>
-            <Grid.Row>
-              <h2>Content</h2>
-            </Grid.Row>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
+    <Grid celled="internally" style={{ height: "100vh" }}>
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <Grid.Row style={{ height: "20%" }}>
+            <WritingTreeHeader />
+          </Grid.Row>
+          <MainMenu />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Grid.Row style={{ height: "10%" }}>
+            <TopMenu />
+          </Grid.Row>
+          <Grid.Row style={{ height: "70%" }}>
+            <h2>Main Content</h2>
+          </Grid.Row>
+          <Grid.Row style={{ height: "20%" }}>
+            <h2>Supplemental Content</h2>
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
