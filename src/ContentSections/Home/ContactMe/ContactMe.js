867a6070d87fe33904d412eb725c3c9813a8f590
Jason M <32662279+ingrey1@users.noreply.github.com>
2021-09-03 23:37:33 -0700

    Update content state management (#3)
    * Introduction Content Started
    
    * update content managment
    
    * Move components to reflect table of contents

---
+++ src/ContentSections/Home/ContactMe/ContactMe.js
@@ -0,0 +1,9 @@
+function ContactMe() {
+  return (
+    <div className="contact-me-container">
+      <h1>Contact Me</h1>
+    </div>
+  );
+}
+
+export default ContactMe;
--- src/Menus/HomeTopMenu/HomeTopMenu.js
+++ src/Menus/HomeTopMenu/HomeTopMenu.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 
-// const items = [
-//   { key: "introduction", active: true, name: "Introduction" },
-//   { key: "methodology", name: "Methodology" },
-//   { key: "usingSite", name: "How To Use This Site" },
-//   { key: "contact", name: "Contact" },
-// ];
-
-export default class TopMenu extends Component {
+export default class HomeTopMenu extends Component {
   state = { activeItem: "Introduction" };
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
@@ -24,7 +17,8 @@ export default class TopMenu extends Component {
           onClick={(e, state) => {
             this.handleItemClick(e, state);
             this.props.setMainContent({
-              mainContent: "Introduction",
+              showTopMenu: true,
+              name: "Home",
               mainContentSection: { name: "Introduction" },
             });
           }}
@@ -35,7 +29,8 @@ export default class TopMenu extends Component {
           onClick={(e, state) => {
             this.handleItemClick(e, state);
             this.props.setMainContent({
-              mainContent: "Methodology",
+              showTopMenu: true,
+              name: "Home",
               mainContentSection: { name: "Methodology" },
             });
           }}
@@ -46,7 +41,8 @@ export default class TopMenu extends Component {
           onClick={(e, state) => {
             this.handleItemClick(e, state);
             this.props.setMainContent({
-              mainContent: "UsingThisSite",
+              showTopMenu: true,
+              name: "Home",
               mainContentSection: { name: "UsingThisSite" },
             });
           }}
@@ -57,7 +53,8 @@ export default class TopMenu extends Component {
           onClick={(e, state) => {
             this.handleItemClick(e, state);
             this.props.setMainContent({
-              mainContent: "ContactMe",
+              showTopMenu: true,
+              name: "Home",
               mainContentSection: { name: "ContactMe" },
             });
           }}
