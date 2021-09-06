df6a4dbfdfff9908e71967764226b6c1a5dbadd9
Jason Mortensen <ingrey1@gmail.com>
2021-09-06 05:11:07 -0700

    Add CopyTextToClipboard component

--- src/ContentSections/Home/Contact/Contact.js
+++ src/ContentSections/Home/Contact/Contact.js
@@ -1,9 +1,12 @@
 import { Header, Container } from "semantic-ui-react";
+import CopyTextToClipboard from "../../../common/components/CopyTextToClipboard/CopyTextToClipboard";
 
 function Contact() {
   return (
     <Container text textAlign="center">
       <Header as="h3">Contact</Header>
+      Email:
+      <CopyTextToClipboard text="ingrey1@gmail.com" />
     </Container>
   );
 }
---
+++ src/common/components/CopyTextToClipboard/CopyTextToClipboard.js
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+import "./CopyTextToClipboard.css";
+import { Button, Icon } from "semantic-ui-react";
+
+export default function CopyTextToClipboard({ text }) {
+  const [copySuccess, setCopySuccess] = useState(false);
+  const textAreaRef = useRef(null);
+
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand("copy");
+    setCopySuccess(true);
+  }
+
+  return (
+    <div id="container">
+      <div>
+        <form>
+          <textarea
+            className="disable-resize"
+            name="email"
+            ref={textAreaRef}
+            value={text}
+            readonly
+          />
+        </form>
+      </div>
+      <div>
+        <Button
+          style={{ marginLeft: "5px" }}
+          onClick={copyToClipboard}
+          icon="copy"
+        ></Button>
+      </div>
+      <div>{copySuccess && <Icon name="check" color="green"></Icon>}</div>
+    </div>
+  );
+}
