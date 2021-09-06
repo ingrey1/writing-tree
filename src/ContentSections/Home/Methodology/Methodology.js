b26d0775f5ec4f0ba7dfa911443d51f9a02ff832
Jason Mortensen <ingrey1@gmail.com>
2021-09-06 06:47:59 -0700

    Add methdology section content

--- src/ContentSections/Home/Methodology/Methodology.js
+++ src/ContentSections/Home/Methodology/Methodology.js
@@ -1,12 +1,34 @@
 import { Header, Container } from "semantic-ui-react";
+import { paragraphs } from "./text";
+import Paragraph from "../../../common/components/Paragraph/Paragraph";
 
 function Methodology() {
-    return (
-      <Container text textAlign="center">
-      <Header as="h3">Methodology</Header>
+  return (
+    <Container text>
+      <Header as="h3" textAlign="center">
+        Methodology
+      </Header>
+      <Header as="h2" textAlign="center">
+        The Purpose of This Site
+      </Header>
+      <Paragraph content={paragraphs[1]} />
+      <Header as="h2" textAlign="center">
+        The Code
+      </Header>
+      <Paragraph content={paragraphs[2]} />
+      <Paragraph content={paragraphs[3]} />
+      <Header as="h2" textAlign="center">
+        Documentation Transparency
+      </Header>
+      <Paragraph content={paragraphs[4]} />
+      <Header as="h2" textAlign="center">
+        My View On References
+      </Header>
+      <Paragraph content={paragraphs[5]} />
+      <Paragraph content={paragraphs[6]} />
+      <Paragraph content={paragraphs[7]} />
     </Container>
-    );
-  }
-  
-  export default Methodology;
-  
+  );
+}
+
+export default Methodology;
