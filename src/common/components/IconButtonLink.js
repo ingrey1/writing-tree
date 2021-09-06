8a66b7ac0d6efbfced0bf98b85c28bf4c7479f93
Jason Mortensen <ingrey1@gmail.com>
2021-09-06 16:30:06 -0700

    fix typo on IconButtonLink component

--- src/common/components/IconButtonLink.js
+++ src/common/components/IconButtonLink.js
@@ -2,7 +2,7 @@ import { Button, Icon } from "semantic-ui-react";
 
 export default function IconButtonLink({ url, iconName, iconSize, color }) {
   return (
-    <Button as="a" hre={url} target="_blank">
+    <Button as="a" href={url} target="_blank">
       <Icon name={iconName} size={iconSize} color={color || "teal"}></Icon>
     </Button>
   );
