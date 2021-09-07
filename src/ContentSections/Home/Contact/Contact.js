9a62eb111be4e07c176c6d57111bcea556ff78bd
Jason Mortensen <ingrey1@gmail.com>
2021-09-07 16:07:52 -0700

    add social link

--- src/ContentSections/Home/Contact/Contact.js
+++ src/ContentSections/Home/Contact/Contact.js
@@ -1,4 +1,5 @@
-import { Header, Container } from "semantic-ui-react";
+import "./Contact.css";
+import { Header, Container, Grid } from "semantic-ui-react";
 import IconButtonLink from "../../../common/components/IconButtonLink";
 import { linkedinUrl } from "../../../common/constants";
 import CopyTextToClipboard from "../../../common/components/CopyTextToClipboard/CopyTextToClipboard";
@@ -7,10 +8,18 @@ function Contact() {
   return (
     <Container text textAlign="center">
       <Header as="h3">Contact</Header>
-      Email: <CopyTextToClipboard text="ingrey1@gmail.com" />
-      <div>
+      <Grid.Row className="email-row">
+        <Header as="h4" name="email">
+          Email
+        </Header>
+        <div className="copy-email-container">
+          <CopyTextToClipboard text="ingrey1@gmail.com" />
+        </div>
+      </Grid.Row>
+      <Grid.Row>
+        <Header as="h4">Social Links</Header>
         <IconButtonLink iconName="linkedin in" url={linkedinUrl} size="huge" />
-      </div>
+      </Grid.Row>
     </Container>
   );
 }
