f587ee00db6eb17ce5066303224986d5f41b5a6e
Jason Mortensen <ingrey1@gmail.com>
2021-09-09 14:57:43 -0700

    add placeholder image for supplemental sections for sitecode

--- src/ContentSections/SiteCode/StartHere/Overview/Intro/SupplementalContent/IntroSupplementalContentRow.js
+++ src/ContentSections/SiteCode/StartHere/Overview/Intro/SupplementalContent/IntroSupplementalContentRow.js
@@ -1,10 +1,12 @@
 import { Grid } from "semantic-ui-react";
 import IntroCode from "./IntroCode";
+import NoSupplementalContent from "../../../../../../common/components/NoSupplementalContent/NoSupplementalContent";
 
 export default function IntroSupplementalContentRow() {
   return (
     <Grid.Row>
-      <IntroCode />
+      {/* <IntroCode /> */}
+      <NoSupplementalContent />
     </Grid.Row>
   );
 }
