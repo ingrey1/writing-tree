289bc4f1a6b2e9260211f3f2950a0c09eef4106c
Jason Mortensen <ingrey1@gmail.com>
2021-09-11 15:32:25 -0700

    complete refactor HomeMenu

--- src/ContentSections/Home/HomeMenu.js
+++ src/ContentSections/Home/HomeMenu.js
@@ -1,96 +1,40 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
+import { generateMenuItems } from "../../utils/components";
+
+const itemNames = ["Introduction", "Methodology", "Contact", "Navigation"];
+const newInnerContents = itemNames.map((name) => {
+  return { name };
+});
 
 export default class HomeMenu extends Component {
   render() {
     const {
-      mainContentSection: { name },
-    } = this.props.mainContent;
+      mainContent,
+      mainContent: {
+        mainContentSection: { name },
+      },
+      setMainContent,
+      supplementalContent,
+      setSupplementalContent,
+    } = this.props;
+
     const activeItem = name;
 
     return (
       <Menu fluid tabular widths={4}>
-        <Menu.Item
-          name="Introduction"
-          active={activeItem === "Introduction"}
-          onClick={(e, state) => {
-            this.props.setMainContent({
-              ...this.props.mainContent,
-              name: "Home",
-              mainContentSection: { name: "Introduction" },
-            });
-            this.props.setSupplementalContent({
-              show: false,
-              expand: false,
-              name: "Home",
-              supplementalContentSection: {
-                name: "Introduction",
-                subsection: "Code",
-              },
-            });
-          }}
-        />
-        <Menu.Item
-          name="Methodology"
-          active={activeItem === "Methodology"}
-          onClick={(e, state) => {
-            this.props.setMainContent({
-              showTopMenu: true,
-              name: "Home",
-              mainContentSection: { name: "Methodology" },
-            });
-            this.props.setSupplementalContent({
-              show: false,
-              expand: false,
-              name: "Home",
-              supplementalContentSection: {
-                name: "Methodology",
-                subsection: "FunFacts",
-              },
-            });
-          }}
-        />
-        <Menu.Item
-          name="Navigation"
-          active={activeItem === "Navigation"}
-          onClick={(e, state) => {
-            this.props.setMainContent({
-              showTopMenu: true,
-              name: "Home",
-              mainContentSection: { name: "Navigation" },
-            });
-            this.props.setSupplementalContent({
-              show: false,
-              expand: false,
-              name: "Home",
-              supplementalContentSection: {
-                name: "Navigation",
-                subsection: "Code",
-              },
-            });
-          }}
-        />
-        <Menu.Item
-          name="Contact"
-          active={activeItem === "Contact"}
-          onClick={(e, state) => {
-            this.props.setMainContent({
-              showTopMenu: true,
-              name: "Home",
-              mainContentSection: { name: "Contact" },
-            });
-
-            this.props.setSupplementalContent({
-              show: false,
-              expand: false,
-              name: "Home",
-              supplementalContentSection: {
-                name: "Contact",
-                subsection: "Code",
-              },
-            });
-          }}
-        />
+        {generateMenuItems({
+          activeItem,
+          itemNames,
+          newInnerContents,
+          newOuterContents: { name: "Home" },
+          mainContent,
+          setMainContent,
+          supplementalContent,
+          setSupplementalContent,
+          newSupplementalInnerContents: newInnerContents,
+          newSupplementalOuterContents: { show: false, name: "Home" },
+        })}
       </Menu>
     );
   }
