e32a72dd8e7f51a2e1b10eae512c078ce004091f
Jason Mortensen <ingrey1@gmail.com>
2021-09-22 21:27:55 -0700

    update open api doc

--- src/ContentSections/WebApis/Examples/Examples/BookHeroApiDocument.js
+++ src/ContentSections/WebApis/Examples/Examples/BookHeroApiDocument.js
@@ -15,6 +15,24 @@ const document = {
       get: {
         operationId: "GET Books",
         summary: "Returns all the Books",
+        parameters: [
+          {
+            in: "query",
+            name: "id",
+            schema: {
+              type: "integer",
+            },
+            description: "Find a book by its primary key/id.",
+          },
+          {
+            in: "query",
+            name: "title",
+            schema: {
+              type: "string",
+            },
+            description: "Find a book by its title.",
+          },
+        ],
         tags: ["Books"],
         responses: {
           200: {
@@ -33,12 +51,36 @@ const document = {
               },
             },
           },
+          400: {
+            description: "Validation error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/validationErrorResponse",
+                },
+                examples: {
+                  unknownQueryParam: {
+                    $ref: "#/components/examples/errors/400/unknownQueryParam",
+                  },
+                },
+              },
+            },
+          },
         },
       },
     },
   },
   components: {
     schemas: {
+      validationErrorResponse: {
+        type: "object",
+        properties: {
+          status_code: { type: "integer", example: " 400" },
+          type: { type: "string", example: "validation_error" },
+          param: { type: "string", example: "price" },
+          message: { type: "string", example: '"price" is not allowed' },
+        },
+      },
       Book: {
         type: "object",
         required: ["title", "about"],
@@ -86,29 +128,44 @@ const document = {
       },
     },
     examples: {
+      errors: {
+        400: {
+          unknownQueryParam: {
+            value: {
+              status_code: 400,
+              type: "validation_error",
+              param: "price",
+              message: '"price" is not allowed',
+            },
+          },
+        },
+      },
       books: {
-        value: [
-          {
-            id: 1,
-            title: "Tigana",
-            about: "Amazing Novel by one of the world's greatest artists",
-            createdAt: "2021-08-27T12:22:23.088Z",
-            updatedAt: "2021-08-27T12:22:23.088Z",
-            authors: [],
-            genres: [],
-            tags: [],
-          },
-          {
-            id: 3,
-            title: "A Song or Arbonne",
-            about: "Amazing Novel by one of the world's greatest writers",
-            createdAt: "2021-08-27T12:25:58.770Z",
-            updatedAt: "2021-08-27T12:25:58.770Z",
-            authors: [],
-            genres: [],
-            tags: [],
-          },
-        ],
+        value: {
+          status_code: 200,
+          data: [
+            {
+              id: 1,
+              title: "Tigana",
+              about: "Amazing Novel by one of the world's greatest artists",
+              createdAt: "2021-08-27T12:22:23.088Z",
+              updatedAt: "2021-08-27T12:22:23.088Z",
+              authors: [],
+              genres: [],
+              tags: [],
+            },
+            {
+              id: 3,
+              title: "A Song or Arbonne",
+              about: "Amazing Novel by one of the world's greatest writers",
+              createdAt: "2021-08-27T12:25:58.770Z",
+              updatedAt: "2021-08-27T12:25:58.770Z",
+              authors: [],
+              genres: [],
+              tags: [],
+            },
+          ],
+        },
       },
     },
   },
