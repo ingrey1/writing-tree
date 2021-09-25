c2793a0947fb1ee3f717aba5584e4441c942206a
Jason Mortensen <ingrey1@gmail.com>
2021-09-24 22:29:23 -0700

    add content for tutorial

--- src/ContentSections/WebApis/Tutorials/VisualizationTool/VisualizationTool.js
+++ src/ContentSections/WebApis/Tutorials/VisualizationTool/VisualizationTool.js
@@ -1,3 +1,17 @@
+import { Container, Header } from "semantic-ui-react";
+import { content } from "./text";
+import Paragraph from "../../../../common/components/Paragraph/Paragraph";
+
 export default function VisualizationTool() {
-  return <div>Visualization Tool</div>;
+  return (
+    <Container text style={{ fontSize: "1.2em" }}>
+      <Header as="h2" textAlign="center">
+        Introduction
+      </Header>
+      <Paragraph content={content.introduction[1]} />
+      <Paragraph content={content.introduction[2]} />
+      <Paragraph content={content.introduction[3]} />
+      <Paragraph content={content.introduction[4]} />
+    </Container>
+  );
 }
---
+++ src/ContentSections/WebApis/Tutorials/VisualizationTool/text.js
@@ -0,0 +1,10 @@
+const content = {
+  introduction: {
+    1: `Because the Open API standard gives us a common way of describing a RESTful web API, it's possible to build tools to visualize that information in different ways. If you check out the "Examples" section under "Web APIs", you will see two different visualizations of the same document: "Swagger UI" and "Redoc".`,
+    2: `Both of these are excellent visualization tools for displaying the content of your API. However, you might find that for your particular project, neither of these tools organizes and styles the API content in the way you want. If that's the case, then you can always build your own visualization tool.`,
+    3: `While it may be difficult to build a tool that's general enough to successfully visualize all the documents that adhere to the Open API standard, we don't need to do anything that complex. In most cases, you have a small set of documents, with some specific styling and behavior you want to implement which the popular tools on the market dont provide.`,
+    4: `In this tutorial, we'll walk through building a simplified visualization tool as a React Component. This will provide an opportunity for you to learn more about the structure of the Open Api document, and set the foundation for building your own custom tool that fits your own visualization needs.`,
+  },
+};
+
+export { content };
