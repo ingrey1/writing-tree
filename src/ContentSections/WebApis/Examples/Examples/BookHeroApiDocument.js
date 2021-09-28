7637889fe1fcbd83ac301326a2c12698dd2fe173
Jason Mortensen <ingrey1@gmail.com>
2021-09-27 19:38:46 -0700

    open api document update

--- src/ContentSections/WebApis/Examples/Examples/BookHeroApiDocument.js
+++ src/ContentSections/WebApis/Examples/Examples/BookHeroApiDocument.js
@@ -11,10 +11,11 @@ const document = {
     },
   ],
   paths: {
-    "/book/": {
+    "/book": {
       get: {
         operationId: "GET Books",
-        summary: "Returns all the Books",
+        summary:
+          "Retrieve books from the Book Hero collection. Use query parameters to retrieve a specific subset of the book collection.",
         parameters: [
           {
             in: "query",
@@ -36,16 +37,16 @@ const document = {
         tags: ["Books"],
         responses: {
           200: {
-            description: "Successful response",
+            description:
+              "A Successful response with a list of the books in the Book Hero collection.",
             content: {
               "application/json": {
                 schema: {
-                  $ref: "#/components/schemas/GetBooksResponse"
-                
+                  $ref: "#/components/schemas/GetBooksResponse",
                 },
                 examples: {
                   books: {
-                    $ref: "#/components/examples/books",
+                    $ref: "#/components/examples/responses/getBooks",
                   },
                 },
               },
@@ -59,8 +60,63 @@ const document = {
                   $ref: "#/components/schemas/ValidationErrorResponse",
                 },
                 examples: {
-                  unknownQueryParam: {
+                  "Unknown Query Parameter": {
                     $ref: "#/components/examples/errors/400/unknownQueryParam",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      post: {
+        operationId: "POST books",
+        summary: "Create a new book in the Book Hero collection.",
+        tags: ["Books"],
+        requestBody: {
+          description:
+            "Object that contains the data used to create a new book in the Book Hero collection.",
+          required: true,
+          content: {
+            "application/json": {
+              schema: { $ref: "#/components/schemas/CreateBookRequestBody" },
+              examples: {
+                "Request Body": {
+                  $ref: "#/components/examples/requestBodies/createBook/theBriefWondrousLifeOfOscarWao",
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: "The newly created Book",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/PostBooksResponse",
+                },
+                examples: {
+                  "The Brief Wondrous Life of Oscar Wao": {
+                    $ref: "#/components/examples/responses/postBooks",
+                  },
+                },
+              },
+            },
+          },
+          400: {
+            description: "Validation error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/ValidationErrorResponse",
+                },
+                examples: {
+                  "Required Request Body Parameter": {
+                    $ref: "#/components/examples/errors/400/requiredParam",
+                  },
+                  "Unique Request Body Parameter": {
+                    $ref: "#/components/examples/errors/400/uniqueParam",
                   },
                 },
               },
@@ -73,19 +129,31 @@ const document = {
   components: {
     schemas: {
       GetBooksResponse: {
-         type: "object",
-         "properties": {
-           "status_code": {
-             "type": "integer",
-             "example": 200
-           },
-           "data": {
-              "type": "array",
-              "items": {
-                $ref: "#/components/schemas/Book"
-              }
-           }
-         }
+        type: "object",
+        properties: {
+          status_code: {
+            type: "integer",
+            example: 200,
+          },
+          data: {
+            type: "array",
+            items: {
+              $ref: "#/components/schemas/Book",
+            },
+          },
+        },
+      },
+      PostBooksResponse: {
+        type: "object",
+        properties: {
+          status_code: {
+            type: "integer",
+            example: 201,
+          },
+          data: {
+            $ref: "#/components/schemas/Book",
+          },
+        },
       },
       ValidationErrorResponse: {
         type: "object",
@@ -94,6 +162,20 @@ const document = {
           type: { type: "string", example: "validation_error" },
           param: { type: "string", example: "price" },
           message: { type: "string", example: '"price" is not allowed' },
+        },
+      },
+      CreateBookRequestBody: {
+        type: "object",
+        required: ["title", "about"],
+        properties: {
+          title: {
+            type: "string",
+            example: "A book title.",
+          },
+          about: {
+            type: "string",
+            example: "A book description.",
+          },
         },
       },
       Book: {
@@ -139,10 +221,29 @@ const document = {
             },
             uniqueItems: true,
           },
+          createdAt: {
+            type: "string",
+            format: "date-time",
+          },
+          updatedAt: {
+            type: "string",
+            format: "date-time",
+          },
         },
       },
     },
     examples: {
+      requestBodies: {
+        createBook: {
+          theBriefWondrousLifeOfOscarWao: {
+            value: {
+              title: "The Brief Wondrous Life of Oscar Wao",
+              about:
+                "A unique blend of cultural critique and coming-of-age story with an interesting Dominican twist.",
+            },
+          },
+        },
+      },
       errors: {
         400: {
           unknownQueryParam: {
@@ -153,33 +254,63 @@ const document = {
               message: '"price" is not allowed',
             },
           },
+          requiredParam: {
+            value: {
+              status_code: 400,
+              type: "validation_error",
+              param: "about",
+              message: '"about" is required',
+            },
+          },
+          uniqueParam: {
+            value: {
+              status_code: 400,
+              type: "validation_error",
+              message: "title must be unique",
+            },
+          },
         },
       },
-      books: {
-        value: {
-          status_code: 200,
-          data: [
-            {
-              id: 1,
-              title: "Tigana",
-              about: "Amazing Novel by one of the world's greatest artists",
-              createdAt: "2021-08-27T12:22:23.088Z",
-              updatedAt: "2021-08-27T12:22:23.088Z",
-              authors: [],
-              genres: [],
-              tags: [],
+      responses: {
+        getBooks: {
+          value: {
+            status_code: 200,
+            data: [
+              {
+                id: 1,
+                title: "Tigana",
+                about: "Amazing Novel by one of the world's greatest artists",
+                createdAt: "2021-08-27T12:22:23.088Z",
+                updatedAt: "2021-08-27T12:22:23.088Z",
+                authors: [],
+                genres: [],
+                tags: [],
+              },
+              {
+                id: 3,
+                title: "A Song or Arbonne",
+                about: "Amazing Novel by one of the world's greatest writers",
+                createdAt: "2021-08-27T12:25:58.770Z",
+                updatedAt: "2021-08-27T12:25:58.770Z",
+                authors: [],
+                genres: [],
+                tags: [],
+              },
+            ],
+          },
+        },
+        postBooks: {
+          value: {
+            status_code: 201,
+            data: {
+              id: 8,
+              title: "The Brief Wondrous Life of Oscar Wao",
+              about:
+                "A unique blend of cultural critique and coming-of-age story with an interesting Dominican twist.",
+              updatedAt: "2021-09-28T02:26:34.443Z",
+              createdAt: "2021-09-28T02:26:34.443Z",
             },
-            {
-              id: 3,
-              title: "A Song or Arbonne",
-              about: "Amazing Novel by one of the world's greatest writers",
-              createdAt: "2021-08-27T12:25:58.770Z",
-              updatedAt: "2021-08-27T12:25:58.770Z",
-              authors: [],
-              genres: [],
-              tags: [],
-            },
-          ],
+          },
         },
       },
     },
