a10090f7ebdfde43106246d8b9e6df1854a89907
Jason Mortensen <ingrey1@gmail.com>
2021-09-27 23:10:49 -0700

    tutorial content update

--- src/ContentSections/WebApis/Tutorials/VisualizationTool/VisualizationTool.js
+++ src/ContentSections/WebApis/Tutorials/VisualizationTool/VisualizationTool.js
@@ -1,9 +1,21 @@
-import { Container, Header, Divider, List } from "semantic-ui-react";
+import {
+  Container,
+  Header,
+  Divider,
+  List,
+  Message,
+  Image,
+} from "semantic-ui-react";
 import { content } from "./text";
-import { CodeBlock } from "@atlaskit/code";
+import { CodeBlock, monokai } from "react-code-blocks";
 import Paragraph from "../../../../common/components/Paragraph/Paragraph";
 import IconButtonLink from "../../../../common/components/IconButtonLink";
 import { visualCodeStudioUrl, nodeUrl } from "../../../../common/constants";
+import visualizationTool1 from "./images/visualization-tool1.png";
+import visualizationTool2 from "./images/visualization-tool2.png";
+import visualizationTool3 from "./images/visualization-tool3.png";
+import visualizationTool4 from "./images/visualization-tool4.png";
+import visualizationTool5 from "./images/visualization-tool5.png";
 
 export default function VisualizationTool() {
   return (
@@ -48,17 +60,78 @@ export default function VisualizationTool() {
       </Header>
       <Paragraph content={content.projectSetup[1]} />
       <Paragraph content={content.projectSetup[2]} />
-      <CodeBlock
-        language="powershell"
-        showLineNumbers={false}
-        text={content.projectSetup[3]}
-      />
+      <Message style={{ backgroundColor: "black", color: "white" }}>
+        <p>{content.projectSetup[3]}</p>
+      </Message>
       <Paragraph content={content.projectSetup[4]} />
+      <Message style={{ backgroundColor: "black", color: "white" }}>
+        <p>{content.projectSetup[5]}</p>
+      </Message>
+      <Paragraph content={content.projectSetup[6]} />
+      <Message style={{ backgroundColor: "black", color: "white" }}>
+        <p>{content.projectSetup[7]}</p>
+      </Message>
+      <Paragraph content={content.projectSetup[8]} />
+      <Image src={visualizationTool1} />
+      <Paragraph content={content.projectSetup[9]} />
+      <Message style={{ backgroundColor: "black", color: "white" }}>
+        <p>{content.projectSetup[10]}</p>
+      </Message>
+      <Paragraph content={content.projectSetup[11]} />
+      <Image src={visualizationTool2} />
+      <Paragraph content={content.projectSetup[12]} />
       <CodeBlock
-        language="powershell"
-        showLineNumbers={false}
-        text={content.projectSetup[5]}
+        language="jsx"
+        highlight="7-9,10-20"
+        theme={monokai}
+        text={content.projectSetup[13]}
       />
+      <Paragraph content={content.projectSetup[14]} />
+      <CodeBlock
+        language="jsx"
+        theme={monokai}
+        text={content.projectSetup[15]}
+      />
+      <Paragraph content={content.projectSetup[16]} />
+      <Image src={visualizationTool3} style={{ border: "2px solid teal" }} />
+      <Paragraph content={content.projectSetup[17]} />
+      <Image src={visualizationTool4} />
+      <Paragraph content={content.projectSetup[18]} />
+      <CodeBlock
+        language="jsx"
+        theme={monokai}
+        text={content.projectSetup[19]}
+      />
+      <Paragraph content={content.projectSetup[20]} />
+      <CodeBlock
+        language="jsx"
+        highlight="2,8"
+        theme={monokai}
+        text={content.projectSetup[21]}
+      />
+      <Paragraph content={content.projectSetup[22]} />
+      <Image src={visualizationTool5} style={{ border: "2px solid teal" }} />
+      <Paragraph content={content.projectSetup[23]} />
+      <Message style={{ backgroundColor: "black", color: "white" }}>
+        <p>{content.projectSetup[24]}</p>
+      </Message>
+      <Paragraph content={content.projectSetup[25]} />
+      <CodeBlock
+        language="jsx"
+        highlight="4"
+        theme={monokai}
+        text={content.projectSetup[26]}
+      />
+      <Paragraph content={content.projectSetup[27]} />
+      <Paragraph content={content.projectSetup[28]} />
+      <Divider />
+      <Header as="h2" textAlign="center">
+        Building The Component
+      </Header>
+      <Header as="h3" textAlign="center">
+        Describing The Component's Features
+      </Header>
+
       <Divider />
       <Header as="h2" textAlign="center">
         To Be Continued...
--- src/ContentSections/WebApis/Tutorials/VisualizationTool/text.js
+++ src/ContentSections/WebApis/Tutorials/VisualizationTool/text.js
@@ -9,11 +9,95 @@ const content = {
     1: `Before we get started, let's make sure that you have the right tools for the job.`,
   },
   projectSetup: {
-    1: `As we will be building our interactive visualization tool as a React component, we'll need a React setup test things out as we go. So, we'll be using the 'create-react-app' npx package to bootstrap our project.`,
+    1: `As we will be building our interactive visualization tool as a React component, we'll need a React setup to test things out as we go. So, we'll be using the 'create-react-app' npx package to bootstrap our project.`,
     2: `Go ahead and verify that you have a version of npx on your machine via the command line (when you installed Node, the node package manager was also installed).`,
     3: `\home> npx -v`,
     4: `Your terminal should show something like this.`,
     5: `6.14.14`,
+    6: `'npx' allows us to make use of the latest version of a package without installing that package locally. Let's create a new React project using 'create-react-app'; we'll name it "visualization-tool".`,
+    7: `home> npx create-react-app visualization-tool`,
+    8: `When the command is finished running, you should have a new folder named 'visualization-tool' in the directory where you ran the command. Go ahead and open that folder in your text editor or IDE. It should look something like this.`,
+    9: `'create-react-app' provided us with a bootstrapped starter web app. Before we worry about any of the code, let's run the app. Enter this command from the 'visualization-tool' directory`,
+    10: `home/visualization-tools> npm start`,
+    11: `This will start the web app in your default browser at 'localhost:3000'.`,
+    12: `Because we're just concerned with building a React component, and not an entire web app, we don't need to worry about the project structure. But we're going to want to clear out some of the React boilerplate. Open the App.js file, and delete the lines 7-20 highlighted below.`,
+    13: `import logo from './logo.svg';
+import './App.css';
+    
+    function App() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+    }
+    
+    export default App;
+    `,
+    14: `Now Let's add an 'H1' element inside our 'App' div`,
+    15: `import logo from './logo.svg';
+    import './App.css';
+    
+    function App() {
+      return (
+        <div className="App">
+          <h1>Open API Visualization Tool</h1>
+        </div>
+      );
+    }
+    
+    export default App;
+    `,
+    16: `Make sure you save your work. 'create-react-app' has hot-reloading by default, so you can see saved changes immediately without having to restart your app.`,
+    17: `There we go. Now let's create the file which will contain our visualization component. In React, by convention, component files are uppercase and camelcase. So, go ahead and add the file 'VisualizationTool.js' to the 'src' directory.`,
+    18: `Open the 'VisualizationTool' file. We'll add a functional Component with some starter text.`,
+    19: `export default function VisualizationTool() {
+      return (
+        <div>
+          <h2>Our Code Will live In This Component</h2>
+        </div>
+      );
+    }`,
+    20: `Now, let's import our new component into the 'App.js' file and use it.`,
+    21: `import './App.css';
+import VisualizationTool from "./VisualizationTool"
+    
+    function App() {
+      return (
+        <div className="App">
+          <h1>Open API Visualization Tool</h1>
+          <VisualizationTool />
+        </div>
+      );
+    }
+    
+    export default App;`,
+    22: `On line 2, we import the functional component we've defined and exported in 'VisualizationTool.js'. On line 8, we use it to create some visualizable content.`,
+    23: `We're almost ready to start working on the Visualization Tool. The last thing we're going to do is install a package that will help us style and organize our code. From the project directory, run this command.`,
+    24: `home/visualization-tool> npm install semantic-ui-react semantic-ui-css`,
+    25: `To make full use of the 'semantic-ui-react' package we just installed, we need to import the 'semantic-ui-css' css file inside the 'index.js' file. Open the 'index.js' file, and add this line of code.`,
+    26: `import React from 'react';
+    import ReactDOM from 'react-dom';
+    import './index.css';
+    import "semantic-ui-css/semantic.min.css";
+    import App from './App';
+    import reportWebVitals from './reportWebVitals';
+    `,
+    27: `You will want to restart the web app at this point. Click on your Terminal, and press "Control + C" and then type "Y" and hit "Enter". Now, run "npm start" again.`,
+    28: `Our setup is now complete. We're ready to start planning and building our visualization component.`,
   },
 };
 
